2ba283a4b44777597460b439c52a7275e7c1cd3c
= <=>
2023-01-20 14:28:58 +0300

    fix: fix import

--- src/commands/moveMouse.ts
+++ src/commands/moveMouse.ts
@@ -1,14 +1,14 @@
 import { down, left, mouse, right, up } from '@nut-tree/nut-js';
 import { CommandHandler } from '../types/CommandHandler';
-import { commands } from '../utils/commandNames';
+import { commandNames } from '../utils/commandNames';
 import { easingFunction } from '../utils/easingFunction';
 import { NoHandlerError } from '../errors/NoHandlerError';
 
 const moveDirectionByCommandName = {
-    [commands.mouseUp]: up,
-    [commands.mouseDown]: down,
-    [commands.mouseLeft]: left,
-    [commands.mouseRight]: right,
+    [commandNames.mouseUp]: up,
+    [commandNames.mouseDown]: down,
+    [commandNames.mouseLeft]: left,
+    [commandNames.mouseRight]: right,
 };
 
 const moveMouse: CommandHandler = async (name: string, args: string[]): Promise<string> => {
